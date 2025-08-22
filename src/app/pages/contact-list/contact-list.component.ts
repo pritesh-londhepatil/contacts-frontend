c2ed147f9cd9f8ea3725de41c0728071508de05a
pritesh-londhepatil <londhepatil.social@gmail.com>
2025-08-22 23:30:12 +0530

    Implement contact editing functionality and loading state
    - Added editContact method in ContactListComponent to open the modal for editing a contact.
    - Updated AddEditContactComponent to handle both adding and editing contacts, with dynamic form title.
    - Introduced loading state in the form submission button to indicate processing.
    - Implemented updateContact method in ApiServiceService to handle contact updates via API.

--- src/app/pages/contact-list/contact-list.component.ts
+++ src/app/pages/contact-list/contact-list.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, effect, inject } from '@angular/core';
 import { ApiServiceService } from '../../shared/services/api/api-service.service';
 import { DialogService } from '../../shared/services/dialog/dialog.service';
 import { AddEditContactComponent } from '../../shared/component/add-edit-contact/add-edit-contact.component';
+import { Contacts } from '../../core/contacts';
 
 @Component({
   selector: 'app-contact-list',
@@ -26,6 +27,16 @@ export class ContactListComponent {
   public addContact() {
     this.dialogService.openModal(AddEditContactComponent, {
       modalId: 'addEditContactModal',
+    });
+  }
+
+  public editContact(contact: Contacts) {
+    console.log('contact list component ', contact);
+    this.dialogService.openModal(AddEditContactComponent, {
+      modalId: 'addEditContactModal',
+      props: {
+        contact: contact,
+      },
     });
   }
 }
